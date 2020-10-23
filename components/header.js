424e029da5c087f3a7143a10793bff5c8adefc31
hellobrian <brianhan@hey.com>
2020-10-22 23:17:36 -0500

    refactor menu to useReducer

--- components/header.js
+++ components/header.js
@@ -1,39 +1,86 @@
-import { useState } from "react";
+import { useReducer, useContext, createContext } from "react";
 import { NavLink } from "./nav-link";
 import styles from "./header.module.css";
 
-export function Header({ className }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
+const MenuContext = createContext(null);
+
+const initialState = { isOpen: false };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "open":
+      return { isOpen: true };
+
+    case "close":
+      return { isOpen: false };
+
+    case "toggle":
+      return { isOpen: !state.isOpen };
+
+    default:
+      throw new Error();
+  }
+}
+
+function useMenu() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const isOpen = state.isOpen;
+  const toggle = () => dispatch({ type: "toggle" });
+  const close = () => dispatch({ type: "close" });
+  const open = () => dispatch({ type: "open" });
+
+  return { isOpen, toggle, close, open };
+}
+
+function MenuButton({ id = "menu-list" }) {
+  const { isOpen, toggle } = useContext(MenuContext);
+
   return (
-    <>
+    <button
+      className={styles.button}
+      aria-expanded={isOpen}
+      aria-controls={id}
+      type="button"
+      onClick={toggle}
+    >
+      {isOpen ? (
+        <span className={styles.close}>×</span>
+      ) : (
+        <span className={styles.open}>☰</span>
+      )}
+      <span className={styles.text}>Menu</span>
+    </button>
+  );
+}
+
+function MenuList({ children, id = "menu-list" }) {
+  const { isOpen } = useContext(MenuContext);
+
+  return (
+    <ul
+      id={id}
+      className={styles.menuList}
+      style={{
+        visibility: isOpen ? "visible" : "hidden",
+        height: isOpen ? "auto" : 0,
+      }}
+    >
+      {children}
+    </ul>
+  );
+}
+
+export function Header({ className }) {
+  const { isOpen, toggle } = useMenu();
+
+  return (
+    <MenuContext.Provider value={{ isOpen, toggle }}>
       <header className={`${styles.header} ${className}`}>
         <nav className={styles.menu}>
-          <button
-            className={styles.button}
-            aria-expanded={isOpen}
-            aria-controls="menu-list"
-            type="button"
-            onClick={toggle}
-          >
-            {isOpen ? (
-              <span className={styles.close}>×</span>
-            ) : (
-              <span className={styles.open}>☰</span>
-            )}
-            <span className={styles.text}>Menu</span>
-          </button>
-
+          <MenuButton></MenuButton>
           <div>LOGO</div>
         </nav>
-        <ul
-          id="menu-list"
-          className={styles.menuList}
-          style={{
-            visibility: isOpen ? "visible" : "hidden",
-            height: isOpen ? "auto" : 0,
-          }}
-        >
+        <MenuList>
           <li>
             <NavLink href="/">
               <a className={styles.link}>Keyboards</a>
@@ -49,8 +96,8 @@ export function Header({ className }) {
               <a className={styles.link}>Keycaps</a>
             </NavLink>
           </li>
-        </ul>
+        </MenuList>
       </header>
-    </>
+    </MenuContext.Provider>
   );
 }

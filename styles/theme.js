e7a645eb25638e658993ccc7c962162f990fb5eb
hellobrian <brianhan@hey.com>
2020-10-28 17:34:38 -0500

    update theme.js

--- styles/theme.js
+++ styles/theme.js
@@ -8,6 +8,21 @@ export default {
   fonts: {
     heading: `"IBM Plex Mono", monospace`,
     body: `"IBM Plex Mono", monospace`,
+    monospace: "Menlo, monospace",
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  letterSpacings: {
+    body: "normal",
+    caps: "0.2em",
   },
   text: {
     default: {
@@ -15,4 +30,6 @@ export default {
       fontFamily: "body",
     },
   },
+  breakpoints: ["400px", "768px"],
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
 };

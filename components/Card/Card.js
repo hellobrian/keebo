84c8e2697f336e39fdf7a6c704fb154317cfab03
hellobrian <brianhan@hey.com>
2020-11-15 22:08:09 -0600

    Add astrolokeys and remove hover animatin

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -22,31 +22,6 @@ const LOREM_IPSUM = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. I
   error delectus odit dolores reprehenderit quasi at, accusantium saepe
   nihil, soluta consequatur?`;
 
-function useHover() {
-  const [isHover, setHover] = React.useState(false);
-  const transitionTimingFunction = "cubic-bezier(.17,.67,.83,.67)";
-  const transitionDuration = "100ms";
-
-  const opacityStyles = {
-    opacity: isHover ? 0 : 1,
-    transitionProperty: "opacity",
-    transitionDuration,
-    transitionTimingFunction,
-  };
-
-  const scaleStyles = {
-    transform: isHover ? "scale(1.05)" : "scale(1)",
-    transitionProperty: "transform",
-    transitionDuration,
-    transitionTimingFunction,
-  };
-
-  const hoverOn = () => setHover(true);
-  const hoverOff = () => setHover(false);
-
-  return { isHover, setHover, opacityStyles, scaleStyles, hoverOn, hoverOff };
-}
-
 export function Card({
   src = TEMP_IMG,
   heading = "Drop Ctrl High-Profile",
@@ -55,7 +30,6 @@ export function Card({
   href = "/",
   artisan = false,
 }) {
-  const { hoverOn, hoverOff, scaleStyles } = useHover();
   const { theme } = useThemeUI();
 
   return (
@@ -68,14 +42,12 @@ export function Card({
         borderTop: ["none", "none", `8px solid ${theme.colors.badge[status]}`],
       }}
     >
-      <AspectRatio ratio={16 / 10}>
+      <AspectRatio ratio={16 / 9}>
         <Link href={href} passHref>
           <a aria-label={`click here to learn more about ${heading}`}>
             <Image
               role="presentation"
               quality={70}
-              onMouseEnter={hoverOn}
-              onMouseLeave={hoverOff}
               src={src}
               width={800}
               height={500}
@@ -83,7 +55,6 @@ export function Card({
                 width: "100%",
                 height: "100%",
                 objectFit: "cover",
-                ...scaleStyles,
               }}
             />
           </a>
@@ -102,8 +73,6 @@ export function Card({
           <a style={{ textDecoration: "none" }}>
             <Heading
               variant="h2"
-              onMouseEnter={hoverOn}
-              onMouseLeave={hoverOff}
               sx={{
                 mb: 3,
                 bg: "black",
@@ -140,13 +109,7 @@ export function Card({
           {text || LOREM_IPSUM}
         </Text>
         <Link href={href} passHref>
-          <CardLink
-            color={theme.colors.badge[status]}
-            onMouseEnter={hoverOn}
-            onMouseLeave={hoverOff}
-          >
-            Read more.
-          </CardLink>
+          <CardLink color={theme.colors.badge[status]}>Read more.</CardLink>
         </Link>
       </Flex>
     </Grid>
